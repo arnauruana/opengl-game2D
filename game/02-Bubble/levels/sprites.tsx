873b381a7db4bb7d4bd1f8de218f5ca71f6704a9
Arnau Ruana <arnau.ruana.i@est.fib.upc.edu>
2020-03-09 23:33:27 +0100

    Add prepared sprites for mapping

---
+++ game/02-Bubble/levels/sprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="sprites" tilewidth="24" tileheight="24" tilecount="2112" columns="32">
+ <image source="../images/sprites.png" width="768" height="1584"/>
+</tileset>
