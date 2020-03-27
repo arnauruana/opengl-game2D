2e221abccb4aad97df79b19f7d833ee074d7764b
Arnau Ruana <arnau.ruana.i@est.fib.upc.edu>
2020-03-27 19:10:46 +0100

    Little improvements

--- game/02-Bubble/Player.cpp
+++ game/02-Bubble/Player.cpp
@@ -5,7 +5,7 @@
 #include <GL/glut.h>
 
 #include "Player.h"
-#include "Game.h"
+#include "Keyboard.h"
 
 
 void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
@@ -14,36 +14,34 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 	sprite = Sprite::createSprite(glm::ivec2(24, 24), glm::vec2(0.20, 0.25), &spritesheet, &shaderProgram);
 	sprite->setNumberAnimations(4);
 	
-		sprite->setAnimationSpeed(MOVE_BACKWARD, 8);
-		sprite->addKeyframe(MOVE_BACKWARD, glm::vec2(0.f, 0.25f));
-		sprite->addKeyframe(MOVE_BACKWARD, glm::vec2(0.2f, 0.25f));
-		sprite->addKeyframe(MOVE_BACKWARD, glm::vec2(0.4f, 0.25f));
-		sprite->addKeyframe(MOVE_BACKWARD, glm::vec2(0.6f, 0.25f));
-		sprite->addKeyframe(MOVE_BACKWARD, glm::vec2(0.8f, 0.25f));
+	sprite->setAnimationSpeed(MOVE_BACKWARD, 8);
+	sprite->addKeyframe(MOVE_BACKWARD, glm::vec2(0.f, 0.25f));
+	sprite->addKeyframe(MOVE_BACKWARD, glm::vec2(0.2f, 0.25f));
+	sprite->addKeyframe(MOVE_BACKWARD, glm::vec2(0.4f, 0.25f));
+	sprite->addKeyframe(MOVE_BACKWARD, glm::vec2(0.6f, 0.25f));
+	sprite->addKeyframe(MOVE_BACKWARD, glm::vec2(0.8f, 0.25f));
 
-		sprite->setAnimationSpeed(MOVE_FORWARD, 8);
-		sprite->addKeyframe(MOVE_FORWARD, glm::vec2(0.f, 0.75f));
-		sprite->addKeyframe(MOVE_FORWARD, glm::vec2(0.2f, 0.75f));
-		sprite->addKeyframe(MOVE_FORWARD, glm::vec2(0.4f, 0.75f));
-		sprite->addKeyframe(MOVE_FORWARD, glm::vec2(0.6f, 0.75f));
-		sprite->addKeyframe(MOVE_FORWARD, glm::vec2(0.8f, 0.75f));
-
-		
-		sprite->setAnimationSpeed(MOVE_LEFT, 8);
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.2f, 0.5f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.4f, 0.5f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.6f, 0.5f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.8f, 0.5f));
-		
-		sprite->setAnimationSpeed(MOVE_RIGHT, 8);
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.f, 0.f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.2f, 0.f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.4f, 0.f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.6f, 0.f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.8f, 0.f));
-		
-		
+	sprite->setAnimationSpeed(MOVE_FORWARD, 8);
+	sprite->addKeyframe(MOVE_FORWARD, glm::vec2(0.f, 0.75f));
+	sprite->addKeyframe(MOVE_FORWARD, glm::vec2(0.2f, 0.75f));
+	sprite->addKeyframe(MOVE_FORWARD, glm::vec2(0.4f, 0.75f));
+	sprite->addKeyframe(MOVE_FORWARD, glm::vec2(0.6f, 0.75f));
+	sprite->addKeyframe(MOVE_FORWARD, glm::vec2(0.8f, 0.75f));
+	
+	sprite->setAnimationSpeed(MOVE_LEFT, 8);
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.2f, 0.5f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.4f, 0.5f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.6f, 0.5f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.8f, 0.5f));
+	
+	sprite->setAnimationSpeed(MOVE_RIGHT, 8);
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.f, 0.f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.2f, 0.f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.4f, 0.f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.6f, 0.f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.8f, 0.f));
+	
 	sprite->changeAnimation(0);
 	tileMapDispl = tileMapPos;
 	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
@@ -53,7 +51,7 @@ void Player::update(int deltaTime)
 {
 	this->sprite->update(deltaTime);
 
-	if (Game::instance().getSpecialKey(GLUT_KEY_LEFT))
+	if (keyboard::skey[GLUT_KEY_LEFT])
 	{
 		if (this->sprite->animation() != MOVE_LEFT)
 		{
@@ -64,10 +62,9 @@ void Player::update(int deltaTime)
 
 		this->posPlayer.x -= 24;
 
-
-		Game::instance().specialKeyReleased(GLUT_KEY_LEFT);
+		keyboard::skey[GLUT_KEY_LEFT] = false;
 	}
-	else if (Game::instance().getSpecialKey(GLUT_KEY_RIGHT))
+	else if (keyboard::skey[GLUT_KEY_RIGHT])
 	{
 		if (this->sprite->animation() != MOVE_RIGHT)
 		{
@@ -78,9 +75,9 @@ void Player::update(int deltaTime)
 
 		this->posPlayer.x += 24;
 
-		Game::instance().specialKeyReleased(GLUT_KEY_RIGHT);
+		keyboard::skey[GLUT_KEY_RIGHT] = false;
 	}
-	else if (Game::instance().getSpecialKey(GLUT_KEY_UP))
+	else if (keyboard::skey[GLUT_KEY_UP])
 	{
 		if (this->sprite->animation() != MOVE_BACKWARD)
 		{
@@ -91,9 +88,9 @@ void Player::update(int deltaTime)
 		
 		this->posPlayer.y -= 24;
 		
-		Game::instance().specialKeyReleased(GLUT_KEY_UP);
+		keyboard::skey[GLUT_KEY_UP] = false;
 	}
-	else if (Game::instance().getSpecialKey(GLUT_KEY_DOWN))
+	else if (keyboard::skey[GLUT_KEY_DOWN])
 	{
 		if (this->sprite->animation() != MOVE_FORWARD)
 		{
@@ -104,7 +101,7 @@ void Player::update(int deltaTime)
 		
 		this->posPlayer.y += 24;
 		
-		Game::instance().specialKeyReleased(GLUT_KEY_DOWN);
+		keyboard::skey[GLUT_KEY_DOWN] = false;
 	}
 	
 	this->sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
